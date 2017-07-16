b5b2a6ca459df22308369525ca7e27122ddddd88
starfighter-ix <ec14370@qmul.ac.uk>
2017-07-16 02:26:18 +0100

    Added functionality for following private users.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import org.brunocvcunha.instagram4j.Instagram4j;
+import org.brunocvcunha.instagram4j.requests.InstagramFollowRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramGetUserFollowersRequest;
+import org.brunocvcunha.instagram4j.requests.InstagramLikeRequest;
 import org.brunocvcunha.instagram4j.requests.InstagramTagFeedRequest;
-import org.brunocvcunha.instagram4j.requests.payload.InstagramFeedItem;
-import org.brunocvcunha.instagram4j.requests.payload.InstagramFeedResult;
+import org.brunocvcunha.instagram4j.requests.payload.*;
+import org.pmw.tinylog.Configurator;
+import org.pmw.tinylog.Level;
+import org.pmw.tinylog.Logger;
+import org.pmw.tinylog.writers.FileWriter;
+import org.pmw.tinylog.writers.Writer;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,20 +22,78 @@ public class Main {
 
     private static String USERNAME = "verd.app";
     private static String PASSWORD = "startouch";
+    private static int LIKE_TIME_INTERVAL = 120000;
 
     public static void main(String[] args) throws Exception {
+        Configurator.defaultConfig()
+                .writer(new FileWriter("verdInstaBotLog.txt"))
+                .level(Level.INFO)
+                .activate();
+
         Instagram4j insta = login();
 
         List<String> hashtags = Arrays.asList("earlyadopter", "futuretech");
 
         for(String hashtag : hashtags) {
+            Logger.info("Scanning posts with hashtag: " + hashtag + ".");
+
             InstagramTagFeedRequest tagFeedRequest = new InstagramTagFeedRequest(hashtag);
             InstagramFeedResult tagFeed = insta.sendRequest(tagFeedRequest);
-            for (InstagramFeedItem feedResult : tagFeed.getItems()) {
-                System.out.println("Post ID: " + feedResult.getPk());
 
+            for (InstagramFeedItem feedResult : tagFeed.getItems()) {
+
+                InstagramUser user = feedResult.getUser(); // user of the post
+                if (user.is_private()) {
+                    Logger.info("User " + user.getUsername() + " is private.");
+                    Logger.info("Following  " + user.getUsername() + ".");
+                    insta.sendRequest(new InstagramFollowRequest(user.getPk())); // send follow request
+                }
+
+                Logger.info("Post " + feedResult.getId() + " has " +feedResult.getLike_count() + " likes.");
+                List<InstagramUserSummary> likers = feedResult.getLikers();
+                for(InstagramUserSummary liker : likers) {
+                    if (user.is_private()) {
+                        Logger.info("User " + user.getUsername() + " is private.");
+                        Logger.info("Following  " + user.getUsername() + ".");
+                        insta.sendRequest(new InstagramFollowRequest(user.getPk())); // send follow request
+                        Thread.sleep(5000);
+                    }
+                }
+
+
+                Logger.info("Post ID: " + feedResult.getPk() + " by user: " + user.getUsername());
+
+                /*
+                 * Issue a like every 25 secs.
+                 */
+                Logger.info("Liking photo with caption: '" + feedResult.getCaption() + "'.");
+                insta.sendRequest(new InstagramLikeRequest(feedResult.getPk())); // like their post.
+
+                // Wait to avoid detection...
+                Logger.info("Sleeping to avoid detection...");
+                Thread.sleep(25000); // 120000
+
+                // Followers of post user
+                InstagramGetUserFollowersResult tagPostUserFollowers =
+                        insta.sendRequest(new InstagramGetUserFollowersRequest(user.getPk()));
+
+                List<InstagramUserSummary> followerUsers = tagPostUserFollowers.getUsers();
+
+                for (InstagramUserSummary followerUser : followerUsers) {
+                    Logger.info(followerUser.getUsername() + " follows " + user.getUsername() + ".");
+
+                    if(followerUser.is_private()) {
+                        Logger.info("User " + followerUser.getUsername() + " is private.");
+                        Logger.info("Following user: " + followerUser.getUsername());
+                        insta.sendRequest(new InstagramFollowRequest(followerUser.getPk())); // send follow request
+                    }
+
+                    Logger.info("Sleeping to avoid detection...");
+                    Thread.sleep(15000);
+                }
             }
         }
+
 
 
 
